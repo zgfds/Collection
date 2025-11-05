c62e867baa8cc9e81841b657fa16251ecb7bbdf5
zgfds <zjatjrtjr777@gmail.com>
2025-11-05 16:24:55 +0900

    Remove execution tips from Main.java
    Removed unnecessary tips for running the code.

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ import java.util.Iterator;
 import java.util.Spliterator;
 import java.util.stream.Stream;
 
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+
 public class Main {
     public static void main(String[] args) {
         ArrayList<Student> students = new ArrayList<>();
